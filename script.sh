90b5725895a2e93715cf15f53454dda4b57cdbaf
4Linux - Free Software Solutions <construtores@4linux.com.br>
2019-11-15 17:05:54 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@
     echo '172.27.11.10 master' >> /etc/hosts
     echo '172.27.11.20 node1' >> /etc/hosts
     echo '172.27.11.30 node2' >> /etc/hosts
-    echo '172.27.11.40 balancer nginx.dexter.com.br nginx1.dexter.com.br nginx2.dexter.com.br' >> /etc/hosts
+    echo '172.27.11.40 balancer nginx.dexter.com.br nginx1.dexter.com.br nginx2.dexter.com.br wordpress.dexter.com.br' >> /etc/hosts
